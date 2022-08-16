7afbca0188ac26d4db167800315becd38c51986d
Nisa <assadiyahkhairunisa@gmail.com>
2022-08-16 10:11:25 +0700

    Login and Regist Testing Demo QA

--- src/main/java/com/juaracoding/pageobject/main/MainApp.java
+++ src/main/java/com/juaracoding/pageobject/main/MainApp.java
@@ -1,5 +1,6 @@
 package com.juaracoding.pageobject.main;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
@@ -14,16 +15,30 @@ public class MainApp {
 		DriverSingleton.getInstance("Chrome");
 		WebDriver driver = DriverSingleton.getDriver();
 		JavascriptExecutor js = (JavascriptExecutor)driver;
-		String url = "https://shop.demoqa.com/my-account/";
+		String url = "https://shop.demoqa.com";
 		driver.get(url);
-		
-		LoginPage loginPage = new LoginPage();
-		loginPage.login("nisaasd","nisa1234");//login adalah method yang ada pada loginpage
 		
 		js.executeScript("window.scrollBy(0,400)");
 		Thread.sleep(500);
+		
+		driver.findElement(By.xpath("//a[normalize-space()='My Account']")).click();
+		Thread.sleep(500);
+		
+		js.executeScript("window.scrollBy(0,400)");
+		Thread.sleep(500);
+		
 		RegistPage registPage = new RegistPage();
 		registPage.regist("nisaasd","nisaaja@gmail.com","Nisaaaja1234");
+		
+		driver.findElement(By.xpath("//a[@class='showlogin']")).click();
+		Thread.sleep(500);
+		
+		driver.findElement(By.xpath("//input[@id='rememberme']")).click();
+		Thread.sleep(500);
+		
+		LoginPage loginPage = new LoginPage();
+		loginPage.login("","nisa1234");//login adalah method yang ada pada loginpage
+		js.executeScript("window, scrollBy(0,400)");
 		
 		delay(3);
 		driver.quit();
